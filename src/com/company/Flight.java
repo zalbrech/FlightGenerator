bf1668abe63156c841bab000023109fd7662e49e
Zach <albrecht.zachary1@gmail.com>
2023-06-25 19:00:10 -0500

    added flightNumber to Flight class

--- src/com/company/Flight.java
+++ src/com/company/Flight.java
@@ -9,16 +9,15 @@ public class Flight {
     private LocalDateTime arrival;
     private double duration;
     private double price;
+    private int flightNumber;
 
-    public Flight(String origin, String destination, LocalDateTime departure, LocalDateTime arrival, double duration, double price) {
+    public Flight(String origin, String destination, LocalDateTime departure, LocalDateTime arrival, double duration, double price, int flightNumber) {
         this.origin = origin;
         this.destination = destination;
         this.departure = departure;
         this.arrival = arrival;
         this.duration = duration;
         this.price = price;
+        this.flightNumber = flightNumber;
     }
-
-
-
 }
