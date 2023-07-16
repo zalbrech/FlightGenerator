4a2d17b096982ce886637cfb9a5e80601499e0a9
Zach <albrecht.zachary1@gmail.com>
2023-07-16 14:42:39 -0500

    populate flight map implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,23 +15,24 @@ public class Main {
     static Random random = new Random();
     private static final Map<String, Airport> airportMap = new HashMap<>();
     private static final List<String> airportList = new LinkedList<>();
+
     public static void main(String[] args) {
 
-        try(Scanner input = new Scanner(new File("src/info.txt")).useLocale(Locale.US);
-            BufferedReader br = new BufferedReader(new FileReader("src/flightTimes.txt"))) {
+        try (Scanner input = new Scanner(new File("src/info.txt")).useLocale(Locale.US);
+             BufferedReader br = new BufferedReader(new FileReader("src/flightTimes.txt"))) {
             String theDelimiter = detectOS();
             input.useDelimiter(theDelimiter); // comma + space delimiter
             String str;
             String[] line;
 
-            while(input.hasNext()) {
+            while (input.hasNext()) {
                 Airport a = createAirportFromFile(input, br);
                 String id = a.getId();
 
                 airportList.add(id);
                 airportMap.put(id, a);
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -57,7 +58,7 @@ public class Main {
 //            System.out.println(abbreviationA + " => " +airportA.getFlightTimes());
 //        }
 //
-//
+//          // DEPRECATED
 //        for(Map.Entry<String, Airport> a : airportMap.entrySet()) {
 //            String abbreviationA = a.getKey();
 //            Airport airportA = a.getValue();
@@ -84,8 +85,7 @@ public class Main {
 //        }
 
         // map to get flight by id
-        Map<Integer, Flight> flightMap = generateFlights(1000);
-
+        Map<Integer, Flight> flightMap = generateFlights(10000);
 
 
 //        try(FileReader fileReader = new FileReader("src/info.txt")) {
@@ -101,155 +101,166 @@ public class Main {
 //        } catch(Exception e) {
 //            e.printStackTrace();
 //          }
-        }
+    }
 
-        // Creates Airport object with data from file
-        // TODO: Overload to allow for manual creation. Will need to move populateFlightTimes to another method
-        public static Airport createAirportFromFile(Scanner input, BufferedReader br) throws IOException {
-            String str = "";
-            String[] line;
-            String id = input.next();
+    // Creates Airport object with data from file
+    // TODO: Overload to allow for manual creation. Will need to move populateFlightTimes to another method
+    public static Airport createAirportFromFile(Scanner input, BufferedReader br) throws IOException {
+        String str = "";
+        String[] line;
+        String id = input.next();
 //                System.out.println(id);
 
-            String name = input.next();
+        String name = input.next();
 //                System.out.println(name);
 
-            String city = input.next();
+        String city = input.next();
 //                System.out.println(city);
 
-            ZoneId timezone = ZoneId.of(input.next());
+        ZoneId timezone = ZoneId.of(input.next());
 //                System.out.println(timezone);
 
-            double latitude = input.nextDouble();
+        double latitude = input.nextDouble();
 //                System.out.println(latitude);
 
-            double longitude = input.nextDouble();
+        double longitude = input.nextDouble();
 //                System.out.println(longitude+"\n\n");
-            Airport a = new Airport(id, name, city, timezone, latitude, longitude);
-            System.out.println(a);
-            if((str = br.readLine()) != null) {
-                line = str.split(",\\s");
-                for (String s : line) {
-                    String[] item = s.split("=");
-                    a.getFlightTimes().put(item[0], Long.parseLong(item[1]));
+        Airport a = new Airport(id, name, city, timezone, latitude, longitude);
+//            System.out.println(a);
+        if ((str = br.readLine()) != null) {
+            line = str.split(",\\s");
+            for (String s : line) {
+                String[] item = s.split("=");
+                a.getFlightTimes().put(item[0], Long.parseLong(item[1]));
 //                    System.out.println(a.getId() + " -> " + item[0] + " " + Long.parseLong(item[1]));
-                }
             }
+        }
 //            System.out.println("\n");
-            return a;
-        }
+        return a;
+    }
 
-        public static void printAirports() {
-            for(Airport a : airportMap.values()) {
-                System.out.println(a);
-            }
-        }
-
-        // used to initialize proper regex to handle difference in line ending syntax
-        public static String detectOS() {
-            return System.getProperty("os.name").charAt(0) == 'W' ? WINDOWS_REGEX : MAC_REGEX;
-        }
-
-
-        // calculate distance in miles using the Haversine Formula
-        public static double calculateDistance(double lat1, double lat2, double long1, double long2) {
-            double latDif = Math.toRadians(lat2 - lat1);
-            double longDif = Math.toRadians(long2 - long1);
-
-            lat1 = Math.toRadians(lat1);
-            lat2 = Math.toRadians(lat2);
-
-            double a = (Math.sin(latDif/2) * Math.sin(latDif/2)) + Math.cos(lat1) * Math.cos(lat2) * ((Math.sin(longDif/2) * Math.sin(longDif/2)));
-            double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-            return EARTH_RADIUS_IN_MILES * c;
-        }
-
-        // calculate flight time based on average speed of 500 mph as well as taxi time
-        public static long calculateFlightTime(Airport a, Airport b) {
-            double airspeed = 500.0 / 60.0;
-            double distance = calculateDistance(a.getLatitude(), b.getLatitude(), a.getLongitude(), b.getLongitude());
-            return Math.round((distance / airspeed) + 40);
-        }
-
-        // TODO: Not implemented
-        public static Map<Integer, Flight> generateFlights(int N) {
-            Set<Integer> flightNumbers = new HashSet<>();
-            Map<Integer, Flight> map = new HashMap<>();
-            int a, b;
-            int len = airportMap.size();
-            Airport[] airportArray = airportMap.values().toArray(new Airport[0]);
-
-            for(int i = 0; i < N; i++) {
-                System.out.println(i);
-                a = random.nextInt(len);
-                b = random.nextInt(len);
-                while(a == b) {
-                    b = random.nextInt(len);
-                }
-
-                Airport originAirport = airportArray[a];
-                System.out.println(originAirport.getId());
-                Airport destinationAirport = airportArray[b];
-                System.out.println(destinationAirport.getId());
-
-                if(originAirport.getFlightTimes().get(destinationAirport.getId()) == null || destinationAirport.getFlightTimes().get(originAirport.getId()) == null) {
-                    continue;
-                }
-
-                LocalDate startDate = LocalDate.now();
-                LocalDate endDate = startDate.plusYears(2);
-
-                LocalTime startTime = LocalTime.of(5,0);
-                LocalTime endTime = LocalTime.of(22,0);
-
-                ZonedDateTime departure = ZonedDateTime.of(createRandomDate(startDate, endDate), createRandomTime(startTime, endTime), originAirport.getTimezone());
-                long theDuration = originAirport.getFlightTimes().get(destinationAirport.getId());
-                System.out.println(theDuration);
-                ZonedDateTime arrival = departure.withZoneSameInstant(destinationAirport.getTimezone()).plusMinutes(theDuration);
-
-                System.out.println(originAirport.getName() + "(" + originAirport.getId() + ") => " +departure);
-                System.out.println("Duration in minutes => " + theDuration);
-                System.out.println(destinationAirport.getName() + "(" + destinationAirport.getId() + ") => " + arrival);
-
-                double thePrice = generatePrice(theDuration, departure.getDayOfYear());
-                System.out.println("price = " + thePrice);
-
-                int flightNumber = generateFlightNumber(90000);
-                while(!flightNumbers.add(flightNumber)) {
-                    flightNumber = generateFlightNumber(90000);
-                }
-            }
-
-
-            return map;
-        }
-
-        // create random date between two defined bounds
-        public static LocalDate createRandomDate(LocalDate startInclusive, LocalDate endInclusive) {
-            long startDate = startInclusive.toEpochDay();
-            long endDate = endInclusive.toEpochDay();
-
-            long randomDate = startDate + (long) (random.nextDouble() * (endDate - startDate));
-
-            return LocalDate.ofEpochDay(randomDate);
-        }
-
-        // create random time between two defined bounds
-        public static LocalTime createRandomTime(LocalTime startInclusive, LocalTime endInclusive) {
-            long startTime = startInclusive.toSecondOfDay();
-            long endTime = endInclusive.toSecondOfDay();
-
-            long randomTime = startTime + (long) (random.nextDouble() * (endTime - startTime));
-
-            return LocalTime.ofSecondOfDay(randomTime).truncatedTo(ChronoUnit.MINUTES);
-        }
-
-        public static int generateFlightNumber(int upperBound) {
-            return random.nextInt(upperBound) + 10000;
-        }
-
-        public static double generatePrice(long theDuration, int departureDay) {
-            return (theDuration * .7) + (90 - (departureDay - LocalDate.now().getDayOfYear())) + random.nextInt(40);
+    public static void printAirports() {
+        for (Airport a : airportMap.values()) {
+            System.out.println(a);
         }
     }
+
+    // used to initialize proper regex to handle difference in line ending syntax
+    public static String detectOS() {
+        return System.getProperty("os.name").charAt(0) == 'W' ? WINDOWS_REGEX : MAC_REGEX;
+    }
+
+
+    // calculate distance in miles using the Haversine Formula
+    public static double calculateDistance(double lat1, double lat2, double long1, double long2) {
+        double latDif = Math.toRadians(lat2 - lat1);
+        double longDif = Math.toRadians(long2 - long1);
+
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+
+        double a = (Math.sin(latDif / 2) * Math.sin(latDif / 2)) + Math.cos(lat1) * Math.cos(lat2) * ((Math.sin(longDif / 2) * Math.sin(longDif / 2)));
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return EARTH_RADIUS_IN_MILES * c;
+    }
+
+    // calculate flight time based on average speed of 500 mph as well as taxi time
+    public static long calculateFlightTime(Airport a, Airport b) {
+        double airspeed = 500.0 / 60.0;
+        double distance = calculateDistance(a.getLatitude(), b.getLatitude(), a.getLongitude(), b.getLongitude());
+        return Math.round((distance / airspeed) + 40);
+    }
+
+    // TODO: Not implemented
+    public static Map<Integer, Flight> generateFlights(int N) {
+        TreeSet<Integer> flightNumbers = new TreeSet<>();
+        TreeSet<Double> prices = new TreeSet<>();
+        Map<Integer, Flight> map = new HashMap<>();
+        int a, b;
+        int len = airportMap.size();
+        Airport[] airportArray = airportMap.values().toArray(new Airport[0]);
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(i);
+            a = random.nextInt(len);
+            b = random.nextInt(len);
+            while (a == b) {
+                b = random.nextInt(len);
+            }
+
+            Airport originAirport = airportArray[a];
+            System.out.println(originAirport.getId());
+            Airport destinationAirport = airportArray[b];
+            System.out.println(destinationAirport.getId());
+
+            if (originAirport.getFlightTimes().get(destinationAirport.getId()) == null ||
+                    destinationAirport.getFlightTimes().get(originAirport.getId()) == null) {
+                continue;
+            }
+
+            LocalDate startDate = LocalDate.now();
+            LocalDate endDate = startDate.plusYears(2);
+
+            LocalTime startTime = LocalTime.of(5, 0);
+            LocalTime endTime = LocalTime.of(22, 0);
+
+            ZonedDateTime departure = ZonedDateTime.of(createRandomDate(startDate, endDate),
+                    createRandomTime(startTime, endTime), originAirport.getTimezone());
+            long theDuration = originAirport.getFlightTimes().get(destinationAirport.getId());
+            System.out.println(theDuration);
+            ZonedDateTime arrival = departure.withZoneSameInstant(destinationAirport.getTimezone()).plusMinutes(theDuration);
+
+            System.out.println(originAirport.getName() + "(" + originAirport.getId() + ") => " + departure);
+            System.out.println("Duration in minutes => " + theDuration);
+            System.out.println(destinationAirport.getName() + "(" + destinationAirport.getId() + ") => " + arrival);
+
+            double thePrice = generatePrice(theDuration, departure.getDayOfYear());
+            System.out.println("price = " + thePrice);
+            prices.add(thePrice);
+
+            int flightNumber = generateFlightNumber(90000);
+            while (!flightNumbers.add(flightNumber)) {
+                flightNumber = generateFlightNumber(90000);
+            }
+
+            Flight flight = new Flight(originAirport,destinationAirport,departure,arrival,theDuration,thePrice,flightNumber);
+
+            map.put(flightNumber,flight);
+        }
+//        debugging only
+//        System.out.println(flightNumbers);
+//        System.out.println(prices);
+
+        return map;
+    }
+
+    // create random date between two defined bounds
+    public static LocalDate createRandomDate(LocalDate startInclusive, LocalDate endInclusive) {
+        long startDate = startInclusive.toEpochDay();
+        long endDate = endInclusive.toEpochDay();
+
+        long randomDate = startDate + (long) (random.nextDouble() * (endDate - startDate));
+
+        return LocalDate.ofEpochDay(randomDate);
+    }
+
+    // create random time between two defined bounds
+    public static LocalTime createRandomTime(LocalTime startInclusive, LocalTime endInclusive) {
+        long startTime = startInclusive.toSecondOfDay();
+        long endTime = endInclusive.toSecondOfDay();
+
+        long randomTime = startTime + (long) (random.nextDouble() * (endTime - startTime));
+
+        return LocalTime.ofSecondOfDay(randomTime).truncatedTo(ChronoUnit.MINUTES);
+    }
+
+    public static int generateFlightNumber(int upperBound) {
+        return random.nextInt(upperBound) + 10000;
+    }
+
+    public static double generatePrice(long theDuration, int departureDay) {
+        int timePremium = (90 - (departureDay - LocalDate.now().getDayOfYear()));
+        return 100 + (Math.round((theDuration * .6) + (Math.max(timePremium, 0)) + random.nextInt(40)));
+    }
+}
 
